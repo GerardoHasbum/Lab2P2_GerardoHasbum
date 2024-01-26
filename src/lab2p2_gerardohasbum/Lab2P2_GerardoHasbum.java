d06b71fa9f602cd5283f47557548d4597887ccf0
GerardoHasbum <ghasbumgo@gmail.com>
2024-01-26 13:30:16 -0600

    Asignacion de variables

---
+++ src/lab2p2_gerardohasbum/Lab2P2_GerardoHasbum.java
@@ -0,0 +1,19 @@
+package lab2p2_gerardohasbum;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Lab2P2_GerardoHasbum {
+    
+public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ArrayList<> Libros = new ArrayList();
+        ArrayList<> Articulos = new ArrayList();
+        ArrayList<> Cursos = new ArrayList();
+        ArrayList<> Conferencias = new ArrayList();
+        ArrayList<> Usuarios = new ArrayList();
+    }
+    
+}
