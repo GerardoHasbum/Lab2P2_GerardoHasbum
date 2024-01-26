4385d35b0c09bcbd1c8ddfdebe2e7ccd8d6e7cd3
GerardoHasbum <ghasbumgo@gmail.com>
2024-01-26 14:28:10 -0600

    Inicio de Sesion

--- src/lab2p2_gerardohasbum/Lab2P2_GerardoHasbum.java
+++ src/lab2p2_gerardohasbum/Lab2P2_GerardoHasbum.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Lab2P2_GerardoHasbum {
-    
-public static Scanner sc = new Scanner(System.in);
+
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         ArrayList<Libros> Libros = new ArrayList();
@@ -14,64 +14,108 @@ public static Scanner sc = new Scanner(System.in);
         ArrayList<Cursos> Cursos = new ArrayList();
         ArrayList<Conferencias> Conferencias = new ArrayList();
         ArrayList<Usuarios> Usuarios = new ArrayList();
-        
-        Usuarios.add(new Usuarios("admin","123","est"));
-        
+
+        Usuarios.add(new Usuarios("stud", "123", "est"));//Usuarios de prueba
+        Usuarios.add(new Usuarios("teach", "456", "tch"));
+        Usuarios.add(new Usuarios("admin", "789", "bib"));
+
         Libros.add(new Libros("Harry Potter and the Goblet of Fire", "J.K. Rowling", 'f', "2000", 's'));
         Articulos.add(new Articulos("Aprecio Familiar", "Discord", "Violencia Intrafamiliar", "solo dios sabe", 's'));
         Cursos.add(new Cursos("Si existo", "Dios", 200, "Youtube"));
         Conferencias.add(new Conferencias("QUE SI EXISTO", "Dios", "1/26/2024", 2, "googlemeets.com/si"));
-        
+
+        char res_us = 's';
+
+        while (res_us == 's' || res_us == 'S') {//comienzo while #1
+            System.out.println("Inicio de Sesion");
+            System.out.println("Ingrese su Usuario");
+            String usuario = sc.next();
+            System.out.println("Ingrese su Password");
+            String password = sc.next();
+            boolean us_ver = false;
+            boolean pass_ver = false;
+
+            for (int i = 0; i < Usuarios.size(); i++) {
+                Usuarios temp = Usuarios.get(i);
+                if (Usuarios.get(i).getNom().equals(usuario)) {
+                    us_ver = true;
+                }
+            }
+            for (int i = 0; i < Usuarios.size(); i++) {
+                Usuarios temp = Usuarios.get(i);
+                if (Usuarios.get(i).getCont().equals(password)) {
+                    pass_ver = true;
+                }
+            }
+
+            while (pass_ver == false || us_ver == false) {
+                System.out.println("El nombre de usuario o el password esta incorrecto porfavor volver a intentar");
+                System.out.println("Ingrese su Usuario");
+                usuario = sc.next();
+                System.out.println("Ingrese su Password");
+                password = sc.next();
+
+                for (int i = 0; i < Usuarios.size(); i++) {
+                    Usuarios temp = Usuarios.get(i);
+                    if (temp.getNom() == usuario) {
+                        us_ver = true;
+                    }
+                }
+                for (int i = 0; i < Usuarios.size(); i++) {
+                    Usuarios temp = Usuarios.get(i);
+                    if (temp.getCont() == password) {
+                        pass_ver = true;
+                    }
+                }
+            }
+        }
+
         PrintLi(Libros);
         PrintAr(Articulos);
         PrintCu(Cursos);
         PrintCon(Conferencias);
         PrintUs(Usuarios);
-        
+
     }
-    
-    public static void PrintLi(ArrayList x){
+
+    public static void PrintLi(ArrayList x) {
         System.out.println("Libros:");
         for (int i = 0; i < x.size(); i++) {
             System.out.println(x.get(i).toString());
         }
-        
+
     }
-    
-    
-    public static void PrintAr(ArrayList x){
+
+    public static void PrintAr(ArrayList x) {
         System.out.println("Articulos:");
         for (int i = 0; i < x.size(); i++) {
             System.out.println(x.get(i).toString());
         }
-        
+
     }
-    
-    
-    public static void PrintCu(ArrayList x){
+
+    public static void PrintCu(ArrayList x) {
         System.out.println("Cursos:");
         for (int i = 0; i < x.size(); i++) {
             System.out.println(x.get(i).toString());
         }
-        
+
     }
-    
-    
-    public static void PrintCon(ArrayList x){
+
+    public static void PrintCon(ArrayList x) {
         System.out.println("Conferencias:");
         for (int i = 0; i < x.size(); i++) {
             System.out.println(x.get(i).toString());
         }
-        
+
     }
-    
-    
-    public static void PrintUs(ArrayList x){
+
+    public static void PrintUs(ArrayList x) {
         System.out.println("Usuarios:");
         for (int i = 0; i < x.size(); i++) {
             System.out.println(x.get(i).toString());
         }
-        
+
     }
-    
+
 }
